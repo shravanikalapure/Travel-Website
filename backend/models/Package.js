8e24f7f71b38a65662f09928b796aa66e03f19a7
Shravani Kalapure <kalapureshravani@gmail.com>
2026-03-16 16:32:31 +0530

    Delete backend directory

--- backend/models/Package.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const packageSchema = new mongoose.Schema({
-    place: String,
-    price: Number,
-    days: Number
-});
-
-module.exports = mongoose.model("Package", packageSchema);
